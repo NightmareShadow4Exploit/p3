6ea61c921aa849f6c84c0f48042a0eb5343a3c91
NightmareShadow4Exploit <157217769+NightmareShadow4Exploit@users.noreply.github.com>
2024-11-16 19:22:27 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -25,4 +25,16 @@ window.scrollTo({
 });
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+});
+
+
+
+
 
